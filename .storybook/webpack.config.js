6808c1661b76c535e4b62faec67e13843929d78c
Yarden Sod-Moriah <yardnsm@gmail.com>
2017-07-03 14:12:41 +0300

    Migrate to Storybook v3

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,6 @@
 const _ = require('lodash/fp');
 const path = require('path');
-const genDefaultConfig = require('@kadira/storybook/dist/server/config/defaults/webpack.config.js');
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
 const wixNodeBuildConfig = require('yoshi/config/webpack.config.storybook');
 
 module.exports = (config, env) => {
