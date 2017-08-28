bf78c2e7b58ee63e6e54d378deb6f0839700b13f
ihork <ihork@wix.com>
2017-08-28 14:43:34 +0300

    Init DatePicker tests

--- src/DatePicker/DatePicker.driver.js
+++ src/DatePicker/DatePicker.driver.js
@@ -1,51 +1,16 @@
-import moment from 'moment';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+import $ from 'jquery';
 
-function datePickerDriverFactory(component, wrapper = document.body) {
-  return {
-    getDatePickerPopup: () => wrapper.querySelector('.react-datepicker'),
-    navigateToNextMonth() {
-      this.getDatePickerPopup().querySelector('.react-datepicker__navigation--next').click();
-    },
-    navigateToPreviousMonth() {
-      this.getDatePickerPopup().querySelector('.react-datepicker__navigation--previous').click();
-    },
-    getCurrentMonth() {
-      const monthFieldContent = this.getDatePickerPopup()
-        .querySelector('.react-datepicker__current-month')
-        .textContent;
-      return moment(monthFieldContent, ['MMMM YYYY']);
-    },
-    getSelectedDate() {
-      return this.getDatePickerInput().value;
-    },
-    getDatePickerInput: () => component.querySelector('input'),
-    showDatePickerModal() {
-      this.getDatePickerInput().click();
-      if (!this.getDatePickerPopup()) {
-        throw new Error(
-          `In order to show modal, DatePicker requires an app to be mounted inside document.body`
-        );
-      }
-    },
-    pickDay(day) {
-      this.getDatePickerPopup().querySelector(`[aria-label='day-${day}']`).click();
-    },
-    selectDate(newDate) {
-      this.showDatePickerModal();
-      const currentMonth = this.getCurrentMonth();
-      const monthDiff = newDate.clone().startOf('month').diff(currentMonth, 'month');
+const datePickerDriverFactory = ({element, wrapper, component}) => {
 
-      for (let i = 0; i < Math.abs(monthDiff); i++) {
-        if (monthDiff > 0) {
-          this.navigateToNextMonth();
-        } else {
-          this.navigateToPreviousMonth();
-        }
-      }
+    const getFirstAvailableOption = () => element.querySelector('[role="option"]:not([class*="outside-month"])')
 
-      this.pickDay(newDate.date());
-    }
-  };
-}
+    return {
+        exists: () => !!element,
+        getFirstAvailableOption
+    };
+};
 
-export {datePickerDriverFactory};
+export default datePickerDriverFactory;
