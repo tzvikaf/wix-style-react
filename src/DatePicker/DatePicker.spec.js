56b549295621260af3fd2da105085edf749d0d8f
ihork <ihork@wix.com>
2017-08-28 16:03:57 +0300

    DatePicker tests for value, dateFormat and dataHook

--- src/DatePicker/DatePicker.spec.js
+++ src/DatePicker/DatePicker.spec.js
@@ -6,6 +6,7 @@ import {datePickerTestkitFactory} from '../../testkit';
 import DatePicker from './DatePicker';
 import {datePickerTestkitFactory as enzymeDatePickerTestkitFactory} from '../../testkit/enzyme';
 import {mount} from 'enzyme';
+import moment from 'moment';
 
 describe('DatePicker', () => {
     const createDriver = createDriverFactory(datePickerDriverFactory);
@@ -20,4 +21,30 @@ describe('DatePicker', () => {
         expect(driver.exists()).toBeTruthy();
     });
 
-})
+    it('should set inputDataHook from props', () => {
+        const driver = createDriver(<DatePicker onChange={onChange} inputDataHook={'dataHook'}/>);
+
+        expect(Boolean(driver.getElementByDataHook('dataHook'))).toBeTruthy();
+    });
+
+    it('should set datePicker disabled from props', () => {
+        const driver = createDriver(<DatePicker onChange={onChange} disabled={true}/>);
+
+        expect(driver.getDatePickerInput().hasAttribute('disabled')).toBeTruthy();
+    });
+
+    it('should show correct value from props', () => {
+        const date = new Date(2017, 9, 2);
+        const driver = createDriver(<DatePicker onChange={onChange} value={moment(date)}/>);
+
+        expect(driver.getDatePickerInputValue()).toBe('10/02/2017');
+    });
+
+    it('should show correct value from props depends on date format', () => {
+        const date = new Date(2017, 9, 2);
+        const driver = createDriver(<DatePicker onChange={onChange} value={moment(date)} dateFormat={'DD/MM/YYYY'}/>);
+
+        expect(driver.getDatePickerInputValue()).toBe('02/10/2017');
+    });
+
+});
