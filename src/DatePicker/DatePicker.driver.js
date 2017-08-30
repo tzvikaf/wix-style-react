cb334beb55758bcb986b3d4de952c853b2436c7a
Vladislav Zhuk <vladislavz@wix.com>
2017-08-30 15:28:21 +0300

    Add test about closing with keyboard, test section already selected date

--- src/DatePicker/DatePicker.driver.js
+++ src/DatePicker/DatePicker.driver.js
@@ -8,14 +8,18 @@ const datePickerDriverFactory = ({element, wrapper}) => {
   const getCalendar = () => element.querySelector('.react-datepicker');
   const getNthDay = n => element.querySelectorAll('[role="option"]:not([class*="outside-month"])')[n];
 
+  const driver = {
+    exists: () => !!element
+  };
+
   const calendarDriver = {
-    exists: () => !!element,
     isVisible: () => !!getCalendar(),
     getNthDay: (n = 0) => getNthDay(n),
     triggerOnNthDay: ({n = 0, trigger, event}) => ReactTestUtils.Simulate[trigger](getNthDay(n), event)
   };
 
   return {
+    driver,
     inputDriver,
     calendarDriver
   };
