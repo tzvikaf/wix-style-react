eb4fc8a81c497387e3438684ea643f955c7953d7
ihork <ihork@wix.com>
2017-08-30 16:36:40 +0300

    Add year dropdown tests

--- src/DatePicker/DatePicker.driver.js
+++ src/DatePicker/DatePicker.driver.js
@@ -7,6 +7,8 @@ const datePickerDriverFactory = ({element, wrapper}) => {
   const inputDriver = inputDriverFactory({element: inputRoot, wrapper});
   const getCalendar = () => element.querySelector('.react-datepicker');
   const getNthDay = n => element.querySelectorAll('[role="option"]:not([class*="outside-month"])')[n];
+  const getYearDropdown = () => element.querySelector('[class$="year-read-view"]');
+  const getNthYear = n => element.querySelectorAll('[class*="year-option"]')[n];
 
   const driver = {
     exists: () => !!element
@@ -14,8 +16,9 @@ const datePickerDriverFactory = ({element, wrapper}) => {
 
   const calendarDriver = {
     isVisible: () => !!getCalendar(),
-    getNthDay: (n = 0) => getNthDay(n),
-    triggerOnNthDay: ({n = 0, trigger, event}) => ReactTestUtils.Simulate[trigger](getNthDay(n), event)
+    clickOnNthDay: (n = 0) => ReactTestUtils.Simulate.click(getNthDay(n)),
+    clickOnYearDropdown: () => ReactTestUtils.Simulate.click(getYearDropdown()),
+    clickOnNthYear: (n = 1) => ReactTestUtils.Simulate.click(getNthYear(n)),
   };
 
   return {
